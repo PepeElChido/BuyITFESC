f493914f32e6991cbf47dedba3203bc02f15956c
cesar <cesareduardoag09@gmial.com>
2023-04-08 12:53:56 -0600

    ando trabajando en el loggin, estaba viendo lo del feed para los productos pero no se ni como hacerlo me dio demasiada amsiedad de lo perro que se ve eso

---
+++ app/src/main/java/com/automatasoftware/buyitfesc/ui/slideshow/registrarse.java
@@ -0,0 +1,14 @@
+package com.automatasoftware.buyitfesc.ui.slideshow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registrarse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrarse1);
+    }
+}
